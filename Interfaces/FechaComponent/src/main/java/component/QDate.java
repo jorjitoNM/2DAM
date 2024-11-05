ccf5ec9b67d0aac991c0698eafc9f3c20103d3be
jorjitoNM <jorge.novillo@educa.madrid.org>
2024-11-05 22:53:27 +0100

    cambios tarde 05/11/2024

--- Interfaces/FechaComponent/src/main/java/component/QDate.java
+++ Interfaces/FechaComponent/src/main/java/component/QDate.java
@@ -2,18 +2,39 @@ package component;
 
 import javax.swing.*;
 import java.time.LocalDate;
+import java.util.Date;
 
 public class QDate extends JPanel {
-    private LocalDate date = LocalDate.of(1990,1,1);
+    private LocalDate date;
+    private JComboBox day;
+    private JComboBox month;
+    private JTextField year;
+    private final Integer[] days = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+    private final String[] months = {"January","February","March","April","May","June","July","August","September","October","November","December"};
 
-    public QDate() {}
+    public QDate() {
+        setSize(500,500);
+         this.date = LocalDate.of(1990,1,1);
+         day = new JComboBox(days);
+         month = new JComboBox(months);
+         year = new JTextField();
+         year.setBounds(50, 50, 150, 20);
+         year.setEditable(true);
+         add(day);
+         add(month);
+         add(year);
+    }
 
     public void setDate (int day, int month, int year) throws QDateException {
         checkDate(day,month,year);
         date = LocalDate.of(year, month, day);
     }
 
-    private void checkDate(int day, int month, int year) throws QDateException {
+    public LocalDate getDate () {
+        return date;
+    }
+
+    private void checkDate (int day, int month, int year) throws QDateException {
         boolean valid = true;
         boolean leap =  false;
         if (day > 31)
@@ -39,4 +60,6 @@ public class QDate extends JPanel {
         if (!valid)
             throw new QDateException();
     }
+
+
 }
