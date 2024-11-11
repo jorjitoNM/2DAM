40ed0a4434824daa6b064bd39cdb86fe39745675
jorjitoNM <153603350+jorjitoNM@users.noreply.github.com>
2024-11-11 10:58:30 +0100

    Delete Moviles directory

--- Moviles/My Application/app/src/main/java/com/example/myapplication/domain/usecases/GetBook.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.domain.usecases;
-
-import com.example.myapplication.data.Repository;
-import com.example.myapplication.domain.model.Book
-
-class GetBook {
-    operator fun invoke(id : Int) : Book = Repository.getBook(id)
-}
--- Moviles/My Application/app/src/main/java/com/example/myapplication/domain/usecases/GetID.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.domain.usecases
-
-import com.example.myapplication.data.Repository
-
-class GetID() {
-    operator fun invoke () : Int {
-        return Repository.getId()
-    }
-}
