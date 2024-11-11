33404339b2b99154d80610741d978f537063f5a9
jorjitoNM <153603350+jorjitoNM@users.noreply.github.com>
2024-11-11 11:01:54 +0100

    Delete AD directory

--- AD/HospitalCRUD/src/main/java/com/hospitalcrud/dao/utilities/Constantes.java
+++
@@ -1,6 +0,0 @@
-package com.hospitalcrud.dao.utilities;
-
-public class Constantes {
-    public static final String SEPARADOR_CSV = ";";
-    public static final String SEPARADOR_SECUNDARIO = "?";
-}
