276e5455cade785e4ce9bb1d64e9a212b8ac4ce6
jorjitoNM <jorge.novillo@educa.madrid.org>
2024-11-13 11:44:08 +0100

    cambios clase 13/11/2024

--- Interfaces/Calculadora/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.calculadora {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.calculadora to javafx.fxml;
-    exports org.example.calculadora;
-}
