c4131fc248902617c32c7b956652aa4965b40afa
jorjitoNM <jorge.novillo@educa.madrid.org>
2024-11-13 11:41:58 +0100

    cambios clase 13/11/2024

--- Interfaces/CalculadoraJavaFX/src/main/java/org/example/calculadorajavafx/HelloController.java
+++ Interfaces/CalculadoraJavaFX/src/main/java/org/example/calculadorajavafx/HelloController.java
@@ -1,10 +1,45 @@
 package org.example.calculadorajavafx;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.util.List;
 
 public class HelloController {
 
-    public void onNumberClicked(ActionEvent actionEvent) {
+    @FXML
+    private TextField caja;
 
+    private List<String> operandos;
+
+    public void onNumberClicked(ActionEvent actionEvent) {
+        Button numero = (Button) actionEvent.getSource();
+
+    }
+    public void onEqualClicked(ActionEvent actionEvent) {
+        if (caja.getCharacters().isEmpty()) {
+            Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+            alerta.setTitle("Error");
+            alerta.setHeaderText(null);
+            alerta.setContentText("Introduzca numeros para operar");
+            alerta.showAndWait();
+        }
+        else {
+            String operation = caja.getText();
+            for (int i = 0; i < operation.length(); i++) {
+                while (isOperator(operation.charAt(i))) {
+
+                }
+            }
+        }
+    }
+
+    private boolean isOperator(char c) {
+        if (c == '+' || c == '-' || c == '*' || c == '/') {
+            return true;
+        }
     }
 }
