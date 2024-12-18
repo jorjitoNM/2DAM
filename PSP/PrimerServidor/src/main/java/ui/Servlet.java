4d7a2711294567260b3b7413bbea7cd1131e679e
jorjitoNM <jorge.novillo@educa.madrid.org>
2024-12-18 13:50:03 +0100

    fuck

--- PSP/PrimerServidor/src/main/java/ui/Servlet.java
+++ PSP/PrimerServidor/src/main/java/ui/Servlet.java
@@ -12,11 +12,23 @@ import java.io.IOException;
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        dispatchRequest(req,resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("holaa");
+        dispatchRequest(req,resp);
+    }
+
+    private void dispatchRequest (HttpServletRequest req, HttpServletResponse resp) {
+        Integer random = (Integer)req.getSession().getAttribute("random");
+
+        Integer intento =(Integer)req.getSession().getAttribute("intentos");
+
+        if (intento == null) {
+            intento = 1;
+            random
+        }
+
     }
 }
