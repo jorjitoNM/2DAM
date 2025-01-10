58727efd9c593f2a5c2dbb3dad611a0ac43af042
jorjitoNM <jorge.novillo@educa.madrid.org>
2025-01-10 12:51:37 +0100

    añadidas las dependencias de Diego (salvo el build.gradle de la aplicacion)

--- Moviles/AppTareas/app/build.gradle.kts
+++ Moviles/AppTareas/app/build.gradle.kts
@@ -2,38 +2,30 @@ import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 import java.util.Properties
 
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
-    alias(libs.plugins.kapt)
-    alias(libs.plugins.hilt)
-    alias(libs.plugins.room)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
     alias(libs.plugins.navigation.safeargs.kotlin)
+    alias(libs.plugins.room)
 }
 
-
 android {
-    namespace = "com.example.primerxmlmvvm"
+    namespace = "com.example.primeraapp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.primerxmlmvvm"
+        applicationId = "com.example.primeraapp"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-        //load the values from .properties file
-//        val keystoreFile = project.rootProject.file("apikey.properties")
-//        val properties = Properties()
-//        properties.load(keystoreFile.inputStream())
-//
-//        //return empty key in case something goes wrong
-//        val apiKey = properties.getProperty("API_KEY") ?: ""
-//        buildConfigField("String", "API_KEY", "\"$apiKey\"")
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+
         buildConfigField("String", "API_URL", "\"https://jsonplaceholder.typicode.com/\"")
     }
-
 
     buildTypes {
         release {
@@ -45,19 +37,20 @@ android {
         }
     }
 
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
 
-    buildFeatures {
-        viewBinding = true
-        buildConfig = true
-
-    }
     room {
         schemaDirectory("$projectDir/schemas")
     }
@@ -67,47 +60,23 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.material)
-    // para el decortator al hacer swipe
-    implementation(libs.recyclerview.swipedecorator)
-
-
-    // Fragments
+    implementation(libs.timber)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-
-    implementation(libs.bundles.viewmodel)
-
-
-
-    implementation(libs.timber)
-
-    //retrofit
-
-    //Retrofit
-    implementation (libs.retrofit)
-    implementation (libs.converter.gson)
-    implementation (libs.converter.scalars)
-    implementation (libs.logging.interceptor)
-
     implementation(libs.coil)
-
-
-    // Hilt
     implementation(libs.hilt.core)
-    kapt(libs.hilt.compiler)
-
-    //Room
+    ksp(libs.hilt.compiler)
+    implementation(libs.bundles.retrofit)
     implementation(libs.androidx.room.runtime)
-    annotationProcessor(libs.androidx.room.compiler)
-    // To use Kotlin Symbol Processing (KSP)
-    ksp(libs.androidx.room.compiler)
-    // optional - Kotlin Extensions and Coroutines support for Room
     implementation(libs.androidx.room.ktx)
-
-    // testing
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
