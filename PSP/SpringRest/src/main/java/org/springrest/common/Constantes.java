4d3aa088be9be4dda427bf70c7a456549245a230
jorjitoNM <jorge.novillo@educa.madrid.org>
2025-02-10 22:43:48 +0100

    arreglos finales entrega PSP

--- PSP/SpringRest/src/main/java/org/springrest/common/Constantes.java
+++ PSP/SpringRest/src/main/java/org/springrest/common/Constantes.java
@@ -1,14 +1,6 @@
 package org.springrest.common;
 
 public class Constantes {
-    public static final String UPDATE = "update";
-    public static final String HOME = "home";
-    public static final String LOGGED = "logged";
-    public static final String DISHES = "dishes";
-    public static final String DISH = "dish";
-    public static final String ERROR = "error";
-    public static final String LOGIN = "login";
-    public static final String CREATED = "created";
     public static final String LOGIN_URL = "/login";
     public static final String CHECK_LOGIN_URL = "/checkLogin";
     public static final String SIGNUP_URL = "/signUp";
