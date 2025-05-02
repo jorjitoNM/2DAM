6aba32a9c0de0a81fe8e9de7a4f5a9e0e3555b6e
jorjitoNM <jorge.novillo@educa.madrid.org>
2025-05-02 21:45:46 +0200

    Creado prueba del selecctor de fotos del TGF

--- Moviles/MusicAppConRest/app/src/main/java/com/example/musicapprest/data/SongsRepository.kt
+++ Moviles/MusicAppConRest/app/src/main/java/com/example/musicapprest/data/SongsRepository.kt
@@ -5,7 +5,7 @@ import com.example.musicapprest.data.remote.datasource.SongsDataSource
 import com.example.musicapprest.domain.model.Song
 import javax.inject.Inject
 
-class SongsRepository @Inject constructor(
+class   SongsRepository @Inject constructor(
     private val songsDataSource: SongsDataSource,
 ) {
     suspend fun getAll() : NetworkResult<List<Song>> = songsDataSource.getAll()
